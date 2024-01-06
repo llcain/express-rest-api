860ecd207d30a364f6f04164c076cb94b0e930e7
llcain <llcain2012@gmail.com>
2024-01-05 20:14:49 -0600

    Created dev script in package.json

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 
 const port = 4000;
 
+app.get('/', (req, res) => {
+    res.send('server is running');
+})
+
 app.listen(port, () => {
     console.log(`listing on port,  ${port}`)
 });
