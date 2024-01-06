c5d6470d130491dcd4bf0f29b6a716f30d1a7ff2
llcain <llcain2012@gmail.com>
2024-01-05 20:31:23 -0600

    Added middleware to server.js file

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const app = express();
 
 const port = 4000;
 
+// middleware
+app.use((req, res, next) => {
+    console.log('Time:', Date.now())
+    next()
+});
+
 app.get('/', (req, res) => {
     res.send('server is running');
 })
