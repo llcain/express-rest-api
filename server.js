0b221178eacdcdcbe05f8794e36f1ee70ec402f4
llcain <llcain2012@gmail.com>
2024-01-05 19:53:11 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+// import express
+
+const express = require('express');
+
+const app = express();
+
+const port = 4000;
+
+app.listen(port, () => {
+    console.log(`listing on port,  ${port}`)
+});
