2c5b52849930f9071e0a73f88ed3d8084ab6ba11
llcain <llcain2012@gmail.com>
2024-01-05 21:11:12 -0600

    Added comments to server.js file

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 // import express
-
 const express = require('express');
 
+// instantiate the express app
 const app = express();
 
+// set our port
 const port = 4000;
 
 // middleware
@@ -15,7 +16,7 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
     res.send('server is running');
 })
-
+// listen on port
 app.listen(port, () => {
     console.log(`listing on port,  ${port}`)
 });
